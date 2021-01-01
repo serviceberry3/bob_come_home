f2f1dda179958da989710d7948c05845fb4499a0
Noah Weiner <noah.weiner@yale.edu>
2021-01-01 13:17:01 -0500

    add login form

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from config import Config
 
 #create application obj as instance of class Flask
 app = Flask(__name__) #name var is predefined, set to name of module in which it's used, used to load template files etc.
+
+#tell flask to read and apply config file
+app.config.from_object(Config)
+
 
 #the app package is defined by the app directory. Routes are the different URLs that the app implements
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -32,3 +32,10 @@ def index():
 
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+
+@app.route('/login')
+def login():
+    #instantiate new LoginForm
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
