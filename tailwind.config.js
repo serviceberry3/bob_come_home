35198e00e173a3c170b41e80f4d8bc04bbb5c400
Noah Weiner <noah.weiner@yale.edu>
2021-01-27 03:30:39 +0000

    style base

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'active'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    transitionDelay: ['responsive', 'hover', 'focus', 'active'],
+    transitionDuration: ['responsive', 'hover', 'focus', 'active'],
+    transitionProperty: ['responsive', 'hover', 'focus', 'active'],
+    transitionTimingFunction: ['responsive', 'hover', 'focus', 'active'],
+    scale: ['responsive', 'hover', 'focus', 'active'],
+    borderColor: ['responsive', 'hover', 'focus', 'active'],
+    fill: ['responsive', 'hover', 'focus', 'active'],
+
+    extend: {},
+  },
+  plugins: [],
+}
